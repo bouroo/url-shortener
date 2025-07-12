b4db9f4a634d6ec076cae40554e5c63f8adefe18
Kawin Viriyaprasopsook <kawin.v@kkumail.com>
2025-07-12 16:38:45 +0700

    ci: fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import cloudflare from '@hono/vite-cloudflare-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    cloudflare({
+      entry: 'functions/_worker.ts',
+    }),
+  ],
 })
